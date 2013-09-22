65ff4feeb071fe7061e15552f02aa0a184640a81
John Murowaniecki <jmurowaniecki@gmail.com>
2013-09-22 04:36:45 -0300

    javascripting and styling

--- app/views/loader.php
+++ app/views/loader.php
@@ -10,35 +10,64 @@
         padding:0;
         border:0;
         overflow:hidden;
+        color:#AAA;
     }
     videos {
         height:0;
         width:0;
     }
+    a {
+        display:block;
+        background:#306090;
+        text-align:center;
+        font-family:monospace;
+        height:42px;
+        font-size:33px;
+        text-decoration:none;
+        padding:10px 0;
+    }
+    a:hover {
+        background:#6090F0;
+        color:#FFF;
+    }
     </style>
 </head>
 <body>
-    <?php
-
-        for ($n=0; $n<100; $n++) {
-        ?>
-        <video src="https://s3.amazonaws.com/recli/timelapse/1-1379636513.webm" width="304" height="224" loop autoplay onclick="this.play()"></video>
-        <?php
-        }
+    <a href="#">
+        Load more nonsense data..
+    </a>
     
-    ?>
+    <div>
+    </div>
+    
     <script>
-        (function(){
+        (function () {
             var videos = document.getElementsByTagName('video'),
-                W = videos[0].width,
-                w = Math.floor(screen.width / 10.3),
+                W = 304,
+                H = 224,
+                w = Math.floor(screen.width / 10),
                 p = w * 100 / W;
-                H = videos[0].height,
-                h = Math.floor(H * p / 100);
-                console.log(W,w,h);
+                h = Math.floor(H * p / 100),
+                A = document.getElementsByTagName('a')[0],
+                C = document.getElementsByTagName('div')[0],
+                v = function (url) {
+                	video = document.createElement('video');
+                	video.src = url;
+                	video.width = w;
+                	video.height = h;
+                	video.loop = video.autoplay = true;
+                	video.onclick = 'this.play()';
+                    C.appendChild(video);
+                };
             for (i in videos) {
                 videos[i].width = w;
                 videos[i].height = h;
+            }
+            
+            A.onclick = function (e) {
+                e.preventDefault();
+                
+                v('https://s3.amazonaws.com/recli/timelapse/1-1379636513.webm');
             }
         })();
     </script>
